cf352cb5a1921166af5fc76ac4ff49350f95c5a7
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2016-06-30 10:29:51 +0900

    add proto-repl dependencies

--- project.clj
+++ project.clj
@@ -4,4 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring "1.5.0"]])
+                 [proto-repl "0.1.2"]
+                 [proto-repl-charts "0.2.0"]
+                 [ring "1.5.0"]]
+  :profiles
+  {:dev {:source-paths ["src" "test"]
+         :dependencies [[org.clojure/tools.namespace "0.2.11"]]}})
