fd3cfe9ab04a023e31d4a7ad3684b402068cd621
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-07-06 23:39:50 +0900

    remove unnecessary dependencies

--- project.clj
+++ project.clj
@@ -4,14 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl "0.1.2"]
-                 [proto-repl-charts "0.2.0"]
                  [ring "1.5.0"]
                  [compojure "1.5.1"]
                  [environ "1.0.3"]]
   :plugins [[lein-environ "1.0.3"]]
   :profiles
   {:dev {:source-paths ["src" "test"]
-         :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                        [prone "1.1.1"]]
+         :dependencies [[prone "1.1.1"]]
          :env {:dev "true"}}})
