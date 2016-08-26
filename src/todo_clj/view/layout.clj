f582ceec14685d9f365b939ef70cd932bb30c5b2
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2016-08-26 11:41:31 +0900

    既存の画面に共通レイアウトを適用

--- src/todo_clj/view/layout.clj
+++ src/todo_clj/view/layout.clj
@@ -7,8 +7,8 @@
     [:title "TODO-clj"]
     (include-css "/css/normalize.css"
                  "/css/papier-1.3.1.min.css"
-                 "/ccs/style.css")
+                 "/css/style.css")
     (include-js "/js/main.js")]
    [:body
-    [:header.top-bar.bg-green.depth-3 "TODO-clj"]
+    [:header.top-bar.bg-blue.depth-3 "TODO-clj"]
     [:main body]]))
